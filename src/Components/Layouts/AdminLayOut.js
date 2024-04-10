a4f20c5a1ce333e9609c3ec8cc70e4d3b7ad42a8
uththaramadu99@gmail.com <uththaramadu99@gmail.com>
2024-04-10 15:42:38 +0530

    import re-correct

--- src/Components/Layouts/AdminLayOut.js
+++ src/Components/Layouts/AdminLayOut.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 import SideBar from "./Layout";
 import { Outlet } from "react-router";
-
+import AdminHeader from '../AdminComponents/Header/AdminHeader';
 
 
 const AdminLayOut = () => {
   return (
     <div>
-   
+   <AdminHeader/>
       <div className="flex gap-10">
         <SideBar/>
         <Outlet/>
