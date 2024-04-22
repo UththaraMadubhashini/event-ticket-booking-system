6535ac621d3ac67959631af47f4ac643352cc3bb
uththaramadu99@gmail.com <uththaramadu99@gmail.com>
2024-04-23 00:09:24 +0530

    re design ImageSlider

--- src/Components/ImageSlider/ImageSlider.js
+++ src/Components/ImageSlider/ImageSlider.js
@@ -1,30 +1,72 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import image1 from '../../Assets/Images/SENEHASAKATA ARUTHAK MAIN BANNER.jpg';
 import image2 from '../../Assets/Images/Thamath-Mathakai-943x540.jpg';
 import image3 from '../../Assets/Images/ahuthi main banner- dance.jpg';
+import image4 from '../../Assets/Images/DWATHA MAIM.jpg';
+import image5 from '../../Assets/Images/01.BASS Cham.jpg';
+import image6 from '../../Assets/Images/Hanthanata-Payana-sanda.jpg';
+
+// Arrow icons
+const NextArrowIcon = () => <div className="arrow-icon"></div>;
+const PrevArrowIcon = () => <div className="arrow-icon"></div>;
+
+// Custom right arrow component
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="slick-arrow slick-next" onClick={onClick}>
+      <NextArrowIcon />
+    </div>
+  );
+}
+
+// Custom left arrow component
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="slick-arrow slick-prev" onClick={onClick}>
+      <PrevArrowIcon />
+    </div>
+  );
+}
 
 const ImageSlider = () => {
-  const images = [image1, image2, image3];
+  const images = [image1, image2, image3, image4, image5, image6];
+  const sliderRef = useRef();
 
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 1000,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: () => {
+      if (sliderRef.current) {
+        sliderRef.current.slickPause();
+      }
+    },
+    afterChange: () => {
+      if (sliderRef.current) {
+        sliderRef.current.slickPlay();
+      }
+    },
   };
 
   return (
-    <Slider {...settings}>
+    <Slider {...settings} ref={sliderRef}>
       {images.map((image, index) => (
         <div key={index}>
           <img 
             src={image} 
             alt={`Image ${index + 1}`} 
-            style={{ width: '1520px', height: '550px' }}
+            style={{ width: '100%', height: '500px' }}
           />
         </div>
       ))}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,73 @@
 import React from 'react';
 import ImageSlider from '../Components/ImageSlider/ImageSlider';
+import { styled, alpha } from '@mui/material/styles';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius, // Rounded border
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: theme.spacing(1),
+  marginRight: theme.spacing(1),
+  marginTop: theme.spacing(2),
+  width: '50%',
+  [theme.breakpoints.up('sm')]: {
+    width: 'auto',
+  },
+  boxShadow: '0 3px 6px rgba(0, 0, 0, 0.1)',
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 const Home = () => {
   return (
-    <div>
+    <>
       <ImageSlider />
-    </div>
+      <Toolbar>
+        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+        </Typography>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </Search>
+      </Toolbar>
+    </>
   );
-};
+}
 
 export default Home;
-
