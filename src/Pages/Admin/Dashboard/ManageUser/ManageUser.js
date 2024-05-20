b134b395826ddf0c753ef90c6f7c875efc340235
uththaramadu99@gmail.com <uththaramadu99@gmail.com>
2024-05-20 23:58:09 +0530

    seat selection calculations

--- src/Pages/Admin/Dashboard/ManageUser/ManageUser.js
+++ src/Pages/Admin/Dashboard/ManageUser/ManageUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SideBar from '../../AdminComponents/SideBar/SideBar';
 import NavBar from '../../AdminComponents/NavBar/NavBar';
 import Box from '@mui/material/Box';
@@ -12,59 +12,36 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
-
-const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
-];
-
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
+import { ref, get } from "firebase/database";
+import { database } from "../../../../firebase-config";
 
 export default function ManageUser() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const usersRef = ref(database, 'users'); 
+      try {
+        const snapshot = await get(usersRef);
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          const formattedData = Object.keys(data).map(key => ({
+            id: key,
+            ...data[key]
+          }));
+          setRows(formattedData);
+        } else {
+          console.log("No data available");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -83,28 +60,38 @@ export default function ManageUser() {
         <SideBar />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
           <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            sx={{ padding: "20px" }}
-          >
-            Users Managments
-          </Typography>
-          <Divider />
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              sx={{ padding: "20px" }}
+            >
+              Users Management
+            </Typography>
+            <Divider />
             <TableContainer sx={{ maxHeight: 440 }}>
               <Table stickyHeader aria-label="sticky table">
                 <TableHead>
                   <TableRow>
-                    {columns.map((column) => (
-                      <TableCell
-                        key={column.id}
-                        align={column.align}
-                        style={{ minWidth: column.minWidth }}
-                      >
-                        {column.label}
-                      </TableCell>
-                    ))}
+                    <TableCell align="left" style={{ minWidth: "100px" }}>
+                      UID
+                    </TableCell>
+                    <TableCell align="left" style={{ minWidth: "100px" }}>
+                      User Name
+                    </TableCell>
+                    <TableCell align="left" style={{ minWidth: "100px" }}>
+                      Role
+                    </TableCell>
+                    <TableCell align="left" style={{ minWidth: "100px" }}>
+                      Email
+                    </TableCell>
+                    <TableCell align="left" style={{ minWidth: "100px" }}>
+                      {/* - It hash display in table */}
+                      Password  
+                    </TableCell>
+                    <TableCell align="left" style={{ minWidth: "100px" }}>
+                      Contact Number
+                    </TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
@@ -112,17 +99,14 @@ export default function ManageUser() {
                     .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                     .map((row) => {
                       return (
-                        <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                          {columns.map((column) => {
-                            const value = row[column.id];
-                            return (
-                              <TableCell key={column.id} align={column.align}>
-                                {column.format && typeof value === 'number'
-                                  ? column.format(value)
-                                  : value}
-                              </TableCell>
-                            );
-                          })}
+                        <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                          <TableCell align="left">{row.id}</TableCell>
+                          <TableCell align="left">{row.username}</TableCell>
+                          <TableCell align="left">{row.role}</TableCell>
+                          <TableCell align="left">{row.email}</TableCell>
+                          <TableCell align="left">{row.Password}</TableCell>
+                          <TableCell align="left">{row.contactNumber}</TableCell> 
+
                         </TableRow>
                       );
                     })}
