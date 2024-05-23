f4b68f3e94e8330c8371a57746f867b1407a7bf3
uththaramadu99@gmail.com <uththaramadu99@gmail.com>
2024-05-23 15:51:15 +0530

    some modifications

--- src/Pages/Payment/Payment.js
+++ src/Pages/Payment/Payment.js
@@ -1,4 +1,3 @@
-// Payment.js
 import React from 'react';
 import Card from 'react-credit-cards';
 import {
@@ -15,6 +14,8 @@ import {
   Box,
   CssBaseline,
   Grid,
+  Snackbar,
+  Alert,
 } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
@@ -29,6 +30,17 @@ class Payment extends React.Component {
     issuer: '',
     focused: '',
     formData: null,
+    errors: {
+      name: '',
+      number: '',
+      expiry: '',
+      cvc: '',
+    },
+    snackbar: {
+      open: false,
+      message: '',
+      severity: 'success',
+    },
   };
 
   handleCallback = ({ issuer }, isValid) => {
@@ -44,25 +56,111 @@ class Payment extends React.Component {
   };
 
   handleInputChange = ({ target }) => {
-    if (target.name === 'number') {
-      target.value = formatCreditCardNumber(target.value);
-    } else if (target.name === 'expiry') {
-      target.value = formatExpirationDate(target.value);
-    } else if (target.name === 'cvc') {
-      target.value = formatCVC(target.value);
+    const { name, value } = target;
+    let formattedValue = value;
+  
+    if (name === 'number') {
+      // Remove any non-digit characters and limit input to 19 digits
+      formattedValue = value.replace(/\D/g, '').slice(0, 19);
+      formattedValue = formatCreditCardNumber(formattedValue);
+    } else if (name === 'expiry') {
+      formattedValue = formatExpirationDate(value);
+    } else if (name === 'cvc') {
+      formattedValue = formatCVC(value);
+    }
+  
+    this.setState({ [name]: formattedValue }, () => {
+      this.validateField(name, formattedValue);
+    });
+  };  
+  
+
+  validateField = (name, value) => {
+    let errors = this.state.errors;
+    
+    switch (name) {
+      case 'name':
+        errors.name = /^[a-zA-Z\s]+$/.test(value) ? '' : 'Name must contain only letters and spaces';
+        break;
+      case 'number':
+        const cardNumberLength = value.replace(/\s/g, '').length;
+        errors.number = (cardNumberLength >= 13 && cardNumberLength <= 19) ? '' : 'Card number must have 13-19 digits';
+        break;
+      case 'expiry':
+        errors.expiry = /^(0[1-9]|1[0-2])\/?([0-9]{2})$/.test(value) ? '' : 'Expiry date is invalid';
+        const today = new Date();
+        const [expMonth, expYear] = value.split('/');
+        const expiryDate = new Date(`20${expYear}`, expMonth - 1);
+        if (expiryDate < today) {
+          errors.expiry = 'Card has expired';
+        }
+        break;
+      case 'cvc':
+        errors.cvc = /^[0-9]{3,4}$/.test(value) ? '' : 'CVC must be 3 or 4 digits';
+        break;
+      default:
+        break;
     }
 
-    this.setState({ [target.name]: target.value });
-  };
+    this.setState({ errors });
+};
+
 
   handleSubmit = (e) => {
     e.preventDefault();
-    alert('You have finished payment!');
-    this.form.reset();
+    const { errors } = this.state;
+
+    if (this.validateForm(errors)) {
+      this.setState({
+        snackbar: {
+          open: true,
+          message: 'You have finished payment!',
+          severity: 'success',
+        },
+      });
+      this.form.reset();
+      this.setState({
+        number: '',
+        name: '',
+        expiry: '',
+        cvc: '',
+        issuer: '',
+        focused: '',
+        formData: null,
+      });
+    } else {
+      this.setState({
+        snackbar: {
+          open: true,
+          message: 'Please fill out the form correctly.',
+          severity: 'error',
+        },
+      });
+    }
+  };
+
+  validateForm = (errors) => {
+    let valid = true;
+    Object.values(errors).forEach((error) => {
+      if (error.length > 0) {
+        valid = false;
+      }
+    });
+    return valid;
+  };
+
+  handleCloseSnackbar = () => {
+    this.setState({
+      snackbar: {
+        open: false,
+        message: '',
+        severity: 'success',
+      },
+    });
   };
 
   render() {
-    const { name, number, expiry, cvc, focused, issuer } = this.state;
+    const { name, number, expiry, cvc, focused, issuer, errors, snackbar } = this.state;
 
     return (
       <ThemeProvider theme={theme}>
@@ -108,6 +206,8 @@ class Payment extends React.Component {
                     autoComplete="cc-name"
                     onChange={this.handleInputChange}
                     onFocus={this.handleInputFocus}
+                    error={errors.name.length > 0}
+                    helperText={errors.name}
                   />
                 </Grid>
                 <Grid item xs={12}>
@@ -121,21 +221,26 @@ class Payment extends React.Component {
                     autoComplete="cc-number"
                     onChange={this.handleInputChange}
                     onFocus={this.handleInputFocus}
+                    error={errors.number.length > 0}
+                    helperText={errors.number}
                   />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    id="expiry"
-                    label="Expiration Date"
-                    name="expiry"
-                    autoComplete="cc-exp"
-                    onChange={this.handleInputChange}
-                    onFocus={this.handleInputFocus}
-                  />
-                </Grid>
+  <TextField
+    variant="outlined"
+    required
+    fullWidth
+    id="expiry"
+    label="MM/YY"
+    name="expiry"
+    autoComplete="cc-exp"
+    onChange={this.handleInputChange}
+    onFocus={this.handleInputFocus}
+    error={errors.expiry.length > 0}
+    helperText={errors.expiry.length > 0 ? errors.expiry :''}
+  />
+</Grid>
+
                 <Grid item xs={12} sm={6}>
                   <TextField
                     variant="outlined"
@@ -147,6 +252,8 @@ class Payment extends React.Component {
                     autoComplete="cc-csc"
                     onChange={this.handleInputChange}
                     onFocus={this.handleInputFocus}
+                    error={errors.cvc.length > 0}
+                    helperText={errors.cvc}
                   />
                 </Grid>
               </Grid>
@@ -162,6 +269,15 @@ class Payment extends React.Component {
             </Box>
           </Box>
         </Container>
+        <Snackbar
+          open={snackbar.open}
+          autoHideDuration={6000}
+          onClose={this.handleCloseSnackbar}
+        >
+          <Alert onClose={this.handleCloseSnackbar} severity={snackbar.severity} sx={{ width: '100%' }}>
+            {snackbar.message}
+          </Alert>
+        </Snackbar>
       </ThemeProvider>
     );
   }
